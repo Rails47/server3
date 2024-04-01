e9abe0b9d792ac713a715e767028dd00faa7b470
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-01 22:46:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 1234);
+
+            while (true)
+            {
+                string time = DateTime.Now.ToString(); 
+                byte[] data = Encoding.UTF8.GetBytes(time);
+                server.Send(data, data.Length, endPoint);
+                Thread.Sleep(1000); 
+            }
+        }
+    }
+}
